338d8aa957c6533f61e99e33e9678cf29132f658
Sianooo <117085166+Sianooo@users.noreply.github.com>
2024-09-16 20:39:27 +0200

    Add files via upload

---
+++ Typy danych/Lekcja1/zadanie1/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean urodznieW1PolowieRoku= true;
+        char ulubionaLitera= 'D';
+        byte pierwszyKomputerWiek = 12;
+        short chrzestPolski=966;
+        int ludzie1930=2000000000;
+        long ludzieTeraz=766000000L;
+        float liczbaE=2.71828f;
+        double Pi= 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428;
+
+        System.out.println(urodznieW1PolowieRoku);
+        System.out.println(ulubionaLitera);
+        System.out.println(pierwszyKomputerWiek);
+        System.out.println(chrzestPolski);
+        System.out.println(ludzie1930);
+        System.out.println(ludzieTeraz);
+        System.out.println(liczbaE);
+        System.out.println(Pi);
+        System.out.println("233 bitow");
+    }
+}
+
+
+//sout i tab=System.out.println("Hello world!")
