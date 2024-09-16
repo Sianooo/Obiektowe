450769f207c680424b7a7033d18539eaf005066c
Sianooo <117085166+Sianooo@users.noreply.github.com>
2024-09-16 20:38:04 +0200

    Add files via upload

---
+++ Switch i petle/switch/switch/src/Main.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Zad 1*/
+        Scanner scanner = new Scanner(System.in);
+        /*
+        int numberOfMonth = scanner.nextInt();
+        switch (numberOfMonth)
+        {
+            case 1:
+            System.out.println("Styczeń");
+                break;
+            case 2:
+                System.out.println("Luty");
+                break;
+            case 3:
+                System.out.println("Marzec");
+                break;
+            case 4:
+                System.out.println("Kwiecień");
+                break;
+            case 5:
+                System.out.println("Maj");
+                break;
+            case 6:
+                System.out.println("Czerwiec");
+                break;
+            case 7:
+                System.out.println("Lipiec");
+                break;
+            case 8:
+                System.out.println("Sierpień");
+                break;
+            case 9:
+                System.out.println("Wrzesień");
+                break;
+            case 10:
+                System.out.println("Październik");
+                break;
+            case 11:
+                System.out.println("Listopad");
+                break;
+            case 12:
+                System.out.println("Grudzień");
+                break;
+            default:
+                System.out.println("Nie ma takiego miesiaca!");
+                break;
+        }
+
+        /*Zad 1_petle*/
+        /*
+         for(int i=2;i<=100;i++) {
+            if(i %2==0)
+                System.out.println(i);
+        }*/
+
+
+        /*Zad 2_petle*/
+        /*
+        for (int i=1;i<=10;i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf(i * j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        /*Zad 3_petle*/
+        /*
+        for (int i=6;i>0;i--) {
+            for (int j=0;j<i;j++) {
+                System.out.printf("* ");
+            }
+            System.out.println();
+        }*/
+
+        /*Zad 5_petle*/
+        /*
+        int[] liczbyParzyste={2,4,6,8,10,12,14,16,18,20};
+        for (int wartosc:liczbyParzyste){
+            System.out.println(wartosc);
+        }*/
+
+        /*Zad 1*/
+
+
+
+
+
+
+    }
+}
